23e0428113e4eb4485ff7a5cdb7d7f32747bec6c
iyke <ikegwimik@gmail.com>
2025-10-17 13:06:26 +0100

    add onboarding flow

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './src/navigation/TabNavigator';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./src/navigation/TabNavigator";
+import { StatusBar } from "expo-status-bar";
+import Onboarding from "./src/components/Onboarding";
 
 export default function App() {
+  const [showOnboarding, setShowOnboarding] = useState(true);
+
+  const handleFinishOnboarding = () => setShowOnboarding(false);
+
   return (
     <>
       <NavigationContainer>
-        <TabNavigator />
+        {showOnboarding ? (
+          <Onboarding onDone={handleFinishOnboarding} />
+        ) : (
+          <TabNavigator />
+        )}
       </NavigationContainer>
       <StatusBar style="dark" />
     </>
