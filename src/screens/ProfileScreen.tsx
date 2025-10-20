19defcd2cadb13b2416bf6ae0b7a6eb8debe429d
iyke <ikegwimik@gmail.com>
2025-10-20 09:21:53 +0100

    add profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Avatar from "../components/profile/Avatar";
+import Details from "../components/profile/Details";
+import Header from "../components/ui/Header";
 
 export default function ProfileScreen() {
   return (
-    <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    ></View>
+    <SafeAreaView style={{ padding: 16, flex: 1 }}>
+      <Header title="Profile" />
+      <Avatar />
+      <Details />
+    </SafeAreaView>
   );
 }
